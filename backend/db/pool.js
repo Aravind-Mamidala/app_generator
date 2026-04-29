f8dbdd9640f043a8acd3f305be3966bb7da88f7c
Aravind-Mamidala <mamidalaaravid40@gmail.com>
2026-04-29 14:39:58 +0530

    added remote db

--- backend/db/pool.js
+++ backend/db/pool.js
@@ -1,11 +1,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "app_generator",
-  password: "MAMI2005", // your correct one
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
